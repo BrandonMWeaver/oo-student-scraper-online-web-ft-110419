f1d5299e562356d137872716f56d34d4233afa2f
Brandon Weaver <a7xncob@gmail.com>
2019-11-19 19:37:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
   
   def self.scrape_index_page(index_url)
     student_hashes = []
-    document = Nokogiri(open(index_url))
+    document = Nokogiri::HTML(open(index_url))
     document.css(".student-card").each { |card|
       student_hashes << { name: card.css(".student-name").text,
                           location: card.css(".student-location").text,
@@ -15,7 +15,7 @@ class Scraper
   end
   
   def self.scrape_profile_page(profile_url)
-    
+    document = Nokogiri::HTML(open(profile_url))
   end
   
 end
