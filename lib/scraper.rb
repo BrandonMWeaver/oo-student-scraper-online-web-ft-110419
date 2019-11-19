0672cacfd96d9af95bf217b20f5bcfbc3054625e
Brandon Weaver <a7xncob@gmail.com>
2019-11-19 19:31:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
     document.css(".student-card").each { |card|
       student_hashes << { name: card.css(".student-name").text,
                           location: card.css(".student-location").text,
-                          profile_url: card.css("a[href]").text }
+                          profile_url: card.css("a[href]") }
     }
     pp student_hashes
   end
