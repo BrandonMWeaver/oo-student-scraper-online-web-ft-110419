2646714580110b667dd2a2184741b090bc888542
Brandon Weaver <a7xncob@gmail.com>
2019-11-19 20:25:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
   
   def self.create_from_collection(students_array)
     students_array.each { |student|
-      
+      student.each { |key, value| self.send(("#{key}="), value) }
+      @@all << self
   end
   
   def add_student_attributes(attributes_hash)
