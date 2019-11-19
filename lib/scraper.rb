f413819d88ea4a66d114fb05ff0d4e8d8e0c4fbe
Brandon Weaver <a7xncob@gmail.com>
2019-11-19 19:08:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,13 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  
   def self.scrape_index_page(index_url)
-    
+    hash = Nokogiri::HTML(open-uri("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
   end
-
+  
   def self.scrape_profile_page(profile_url)
     
   end
-
+  
 end
-
